8a2b6487da29450114323335bd6855cab535d00a
Robin Weston <robin.weston@cettire.com>
2026-08-17 13:51:31 +0800

    Initialize fnm in gtr post-create hook

--- dotfiles/.zsh/gtr-post-create.sh
+++ dotfiles/.zsh/gtr-post-create.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # gtr postCreate hook: install correct Node version and dependencies if applicable
 
+if [ -f .nvmrc ] || [ -f .node-version ] || [ -f package.json ]; then
+    eval "$(fnm env --shell bash)"
+fi
+
 if [ -f .nvmrc ] || [ -f .node-version ]; then
     fnm use --install-if-missing --silent-if-unchanged
 fi
